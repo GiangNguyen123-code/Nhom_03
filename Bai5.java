3afc763e28aea0ea4a87429737b033ef71cf39bb
Nguyễn Đăng Mạnh Cường <manhcuong2322006@gmail.com>
2025-09-20 19:12:27 +0700

    btt4

---
+++ Bai5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Bai5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isPrime = n > 1;
+        for (int i = 2; i * i <= n && isPrime; i++) if (n % i == 0) isPrime = false;
+        System.out.println(isPrime ? "YES" : "NO");
+        sc.close();
+    }
+}
