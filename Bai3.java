dbeb6c694b8984b22c37a830719f38ca22a59f9f
Minh Hai <phai86528@gmail.com>
2025-09-20 16:11:03 +0700

    Bai tap tuan 4

--- Bai3.java
+++ Bai3.java
@@ -1,15 +1,42 @@
+/*Viết chương trình thực hiện các phép tính cơ bản (cộng, trừ, nhân, chia) dựa trên lựa chọn của người dùng.*/
 import java.util.Scanner;
-public class PhepToanHaiSo{
+public class Bai3{
+    public static double basicOperators(Scanner sc, double x, double y){
+        System.out.print("1.Phep cong\n2.Phep tru\n3.Phep nhan\n4.Phep chia\n");
+        int choice = sc.nextInt();
+        double z;
+            switch(choice){
+                case 1:
+                    z = x + y;
+                    break;
+                case 2:
+                    z = x - y;
+                    break;
+                case 3:
+                    z = x * y;
+                    break;
+                case 4:
+                    if(y == 0){
+                        System.out.println("Khong chia duoc cho 0");
+                        z = Double.NaN;
+                    }else{
+                        z = x / y;
+                    }
+                    break;
+                default:
+                    System.out.println("Nhap sai!");
+                    z = Double.NaN;
+                    break;
+            }
+        return z;
+    }
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt(),b=sc.nextInt();
-        System.out.println("Tong: "+(a+b));
-        System.out.println("Hieu: "+(a-b));
-        System.out.println("Tich: "+(a*b));
-        if(b!=0){
-            System.out.println("Thuong: "+(a/b));
-            System.out.println("Phan du: "+(a%b));
-        }else System.out.println("Khong the chia cho 0");
-        sc.close();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Nhap 2 so:");
+    double x = sc.nextDouble();
+    double y = sc.nextDouble();
+    double z = basicOperators(sc,x,y);
+    System.out.println("Ket qua la: " + z);
+    sc.close();
     }
 }
