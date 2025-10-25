fdc01fc8b0c6ca415150a2b17b53f99d1fae0320
Nguyễn Đăng Mạnh Cường <134670967+NGUYENDANGMANHCUONG@users.noreply.github.com>
2025-10-25 22:06:23 +0700

    Delete BaiTapTuan5_ManhCuong directory

--- BaiTapTuan5_ManhCuong/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Sach sach1 = new Sach("001", "Lap trinh Java", "Nguyen Van A", 1999, 23);
-        sach1.hienThiThongTin();
-        Sach sach2 = new Sach("002", "Java", "Nguyen A", 2020, 25);
-        sach2.hienThiThongTin();
-    }
-}
