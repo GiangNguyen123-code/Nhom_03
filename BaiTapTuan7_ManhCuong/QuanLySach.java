1195320c7eabf212705d6e7484317c670f73cb1d
Nguyễn Đăng Mạnh Cường <134670967+NGUYENDANGMANHCUONG@users.noreply.github.com>
2025-10-25 20:10:34 +0700

    Add files via upload

---
+++ BaiTapTuan7_ManhCuong/QuanLySach.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class QuanLySach {
+    private ArrayList<Sach> danhSach = new ArrayList<>();
+
+    public void themSach(Sach sach) {
+        danhSach.add(sach);
+        System.out.println("Da them sach: " + sach.getTieuDe());
+    }
+
+    public void hienThiTatCa() {
+        System.out.println("\n===== DANH SACH SACH =====");
+        for (Sach s : danhSach) {
+    System.out.println(s.toString());
+    System.out.println("Gia ban uoc tinh: " + s.tinhGiaBan() + " VND");
+    System.out.println("-----------------------------");
+        }
+    }
+}
