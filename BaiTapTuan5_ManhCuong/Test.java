78726b0cabfbbb918679187249951b86ac40a26e
Nguyễn Đăng Mạnh Cường <134670967+NGUYENDANGMANHCUONG@users.noreply.github.com>
2025-10-25 22:00:23 +0700

    Add files via upload

---
+++ BaiTapTuan5_ManhCuong/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Sach sach1 = new Sach("001", "Lap trinh Java", "Nguyen Van A", 1999, 23);
+        sach1.hienThiThongTin();
+        Sach sach2 = new Sach("002", "Java", "Nguyen A", 2020, 25);
+        sach2.hienThiThongTin();
+    }
+}
