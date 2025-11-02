199f6c005df8e5878b6fcb78d546154ace6a54f2
Huu Giang <nguyendohuugiang2006@gmail.com>
2025-11-02 16:26:00 +0700

    finalbookmanager

---
+++ BaiTapTuan9_HuuGiang/IKiemKe.java
@@ -0,0 +1,7 @@
+package BookManager;
+
+public interface IKiemKe {
+    boolean kiemTraTonKho(int soLuongToiThieu);
+
+    void capNhatViTri(String viTriMoi);
+}
---
+++ BaiTapTuan9_HuuGiang/IQuanLySach.java
@@ -0,0 +1,27 @@
+package BookManager;
+
+public interface IQuanLySach {
+    public void themMoiSach();
+
+    public void timKiemSach();
+
+    public void xoaSach();
+
+    public void hienThiDanhSach();
+
+    public void capnhatSach();
+
+    public void kiemKeTonKho(String maSach);
+
+    public void capNhatViTriSach(String maSach);
+
+    public void tinhGiaBan();
+
+    public void hienThiDanhSachSachGiaoTrinh();
+
+    public void hienThiDanhSachSachTieuThuyet();
+
+    public void sapXepSachTheoGiaBan();
+
+    public void hienThiThongTinSachTheoYeuCau(String yeuCau);
+}
